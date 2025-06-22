f7e1a47d123e4eac4799b18ca210499613e735ee
Aaron <piafyoyo06@gmail.com>
2025-06-22 23:22:55 +0800

    feat: 引入 tailwindcss 模組與修改網站 meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/fonts']
-})
+
+  // 網站 meta 設定
+  app: {
+    head: {
+      title: "2024 切版直播班 - 內容訂閱網站",
+      htmlAttrs: {
+        lang: "zh-Hant",
+      },
+    },
+  },
+  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxtjs/tailwindcss"],
+});
