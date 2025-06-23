ebe6d2d56a3f6f9b9ffe57f661c8ffa56e22a1b0
Aaron <piafyoyo06@gmail.com>
2025-06-24 01:18:04 +0800

    style: 文字反白顏色

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-primary-100">
+  <div class="bg-primary-100 selection:bg-accent-200 selection:text-white">
     <NuxtRouteAnnouncer />
     <NuxtPage />
     <LayoutFooter />
