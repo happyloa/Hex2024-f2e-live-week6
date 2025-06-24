c6af60cfa7a98bf45fbf002b26c02f658c36e800
Aaron <piafyoyo06@gmail.com>
2025-06-25 07:11:29 +0800

    feat: 熱門主題區塊

--- pages/index.vue
+++ pages/index.vue
@@ -20,4 +20,56 @@ useSeoMeta({
       <AtomButton text="馬上訂閱" to="/plans" hasIcon />
     </div>
   </section>
+  <!-- 熱門主題 -->
+  <section class="px-3 py-12 md:py-20">
+    <div class="mx-auto max-w-[1296px]">
+      <h2 class="mb-6 text-h2-sm font-black text-neutral-500 md:text-h2">
+        熱門主題
+      </h2>
+      <ul
+        class="grid grid-cols-1 gap-2 text-center text-h6 font-bold text-white sm:grid-cols-2 sm:text-h5 sm:font-black lg:grid-cols-4 lg:gap-6"
+      >
+        <li
+          class="h-[84px] content-center rounded-br-[20px] rounded-tl-[20px] bg-[url(/images/mobile/home/theme-1.webp)] bg-cover bg-center bg-no-repeat sm:h-[200px] sm:bg-[url(/images/desktop/home/theme-1.webp)]"
+        >
+          <h3>城市漫遊者</h3>
+        </li>
+        <li
+          class="h-[84px] content-center rounded-br-[20px] rounded-tl-[20px] bg-[url(/images/mobile/home/theme-3.webp)] bg-cover bg-center bg-no-repeat sm:h-[200px] sm:bg-[url(/images/desktop/home/theme-3.webp)]"
+        >
+          <h3>秘境尋蹤人</h3>
+        </li>
+        <li
+          class="h-[84px] content-center rounded-br-[20px] rounded-tl-[20px] bg-[url(/images/mobile/home/theme-5.webp)] bg-cover bg-center bg-no-repeat sm:h-[200px] sm:bg-[url(/images/desktop/home/theme-5.webp)]"
+        >
+          <h3>品味設計家</h3>
+        </li>
+        <li
+          class="h-[84px] content-center rounded-br-[20px] rounded-tl-[20px] bg-[url(/images/mobile/home/theme-8.webp)] bg-cover bg-center bg-no-repeat sm:h-[200px] sm:bg-[url(/images/desktop/home/theme-8.webp)]"
+        >
+          <h3>美食探險記</h3>
+        </li>
+        <li
+          class="h-[84px] content-center rounded-br-[20px] rounded-tl-[20px] bg-[url(/images/mobile/home/theme-2.webp)] bg-cover bg-center bg-no-repeat sm:h-[200px] sm:bg-[url(/images/desktop/home/theme-2.webp)]"
+        >
+          <h3>美學建築誌</h3>
+        </li>
+        <li
+          class="h-[84px] content-center rounded-br-[20px] rounded-tl-[20px] bg-[url(/images/mobile/home/theme-4.webp)] bg-cover bg-center bg-no-repeat sm:h-[200px] sm:bg-[url(/images/desktop/home/theme-4.webp)]"
+        >
+          <h3>慢活生活行</h3>
+        </li>
+        <li
+          class="h-[84px] content-center rounded-br-[20px] rounded-tl-[20px] bg-[url(/images/mobile/home/theme-6.webp)] bg-cover bg-center bg-no-repeat sm:h-[200px] sm:bg-[url(/images/desktop/home/theme-6.webp)]"
+        >
+          <h3>工藝匠心賞</h3>
+        </li>
+        <li
+          class="h-[84px] content-center rounded-br-[20px] rounded-tl-[20px] bg-[url(/images/mobile/home/theme-7.webp)] bg-cover bg-center bg-no-repeat sm:h-[200px] sm:bg-[url(/images/desktop/home/theme-7.webp)]"
+        >
+          <h3>時尚先鋒派</h3>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
